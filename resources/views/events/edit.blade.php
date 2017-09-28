e82ca60e8e044bcbd89fa487212698d860438ec9
jawsofdoom <kbell140@gmail.com>
2017-09-28 18:29:27 -0400

    new branch

---
+++ resources/views/events/edit.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('title', '| Events')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <h1>Update an event</h1>
+            <hr>
+            {!! Form::model($event, ['route' => ['events.update', $event->id], 'method' => 'PUT']) !!}
+            {{Form::label('event_name','Event Name')}}
+            {{Form::text('event_name', $event->name, array('class' => 'form-control') )}}
+
+            {{Form::label('event_type','Event Type')}} <br>
+            {{Form::select('event_type', ['Volunteer' => 'Volunteer', 'Reunion' => 'Reunion', 'Community Event' => 'Community Event'], $event->type)}}
+            <br>
+            {{Form::label ('event_date', 'Event Date')}}
+            {{Form::date('event_date', $event->date, array('class' => 'form-control') )}}
+            <br>
+            {{Form::label ('event_time', 'Event Time')}}
+            {{Form::time('event_time', $event->time, array('class' => 'form-control') )}}
+
+            {{Form::submit('Save Changes', array('class' => 'btn btn-success btn-lg btn-block', 'style' => 'margin-top: 20px'))}}
+            {!! Html::linkRoute('events.index', 'Cancel', array(), array('class' => "btn btn-danger btn-lg btn-block", 'style' => 'margin-top: 20px')) !!}
+
+
+            {!! Form::close() !!}
+        </div>
+    </div>
+@endsection
---
+++ resources/views/events/show.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.app')
+
+@section('title', '| Events')
+
+@section('content')
+    <div class="row">
+
+        <div class="col-md-8 col-md-offset-2">
+            <h1>Event Details</h1>
+            <table class="table">
+                <thead>
+                    <th>Event Name</th>
+                    <th>Event Type</th>
+                    <th>Event Date</th>
+                    <th>Event Time</th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td> {{$event->name}}</td>
+                        <td> {{$event->type}}</td>
+                        <td> {{$event->date}}</td>
+                        <td> {{$event->time}}</td>
+                        <td>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    {{ Form::open(['method' => 'GET', 'route' => ['events.edit', $event->id]]) }}
+                                    {{ Form::button('<i class="glyphicon glyphicon-pencil"></i> Edit', array('type' => 'submit', 'class' => 'btn btn-warning')) }}
+                                    {{ Form::close() }}
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    {{ Form::open(['method' => 'DELETE', 'route' => ['events.destroy', $event->id], ]) }}
+                                    {{ Form::button('<i class="glyphicon glyphicon-trash"></i> Delete', array('type' => 'submit', 'data-id' => $event->id, 'class' => 'btn btn-danger', 'onclick' => "return confirm('Are you sure?')")) }}
+                                    {{ Form::close() }}
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+@endsection
