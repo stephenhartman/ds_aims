9e82f0fedd8ee5f974a06a3f79895c5e5174b75e
stephenhartman <stephen.c.hartman@gmail.com>
2017-09-28 18:46:14 -0400

    Add routes

--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,16 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get(
+    '/', function () {
+        return view('welcome');
+    }
+);
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
 Route::resource('posts', 'PostController');
+
+Route::resource('/users', 'UserController', ['only' => ['index', 'show']]);
