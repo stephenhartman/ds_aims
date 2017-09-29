51aa4000e0d71a05e80f786fe3594004e2473582
stephenhartman <stephen.c.hartman@gmail.com>
2017-09-29 12:06:25 -0400

    Add bootstrap datatables css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.16/css/dataTables.bootstrap.min.css">
 </head>
 <body>
     <div id="app">
