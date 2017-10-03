993b8d4cd484f4ec9274f31358fe557917d02249
stephenhartman <stephen.c.hartman@gmail.com>
2017-10-03 10:44:06 -0400

    Clean up post view

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,45 +1,36 @@
 @extends('home')
 
-@section('title', '| Create New Post')
+@section('title', 'Create Post')
 
 @section('content')
 
-<div class ="row">
-	<div class="col-md-8 col-md-offset-2">
+    <div class ="row">
+        <div class="col-md-8 col-md-offset-2">
 
-	
-	<h1><img class="img img-fluid img-rounded mx-auto" src="{{url('/images/lion.jpg')}}" alt="Image"/>Volunteer information - Create New Post</h1>
-	<p>You may enter a description of what the volunteer is donating to the school</p>
-	<hr>
-	
-	
-	
-	
-	
-	
 
-	
-	
-	
-	{!! Form::open(array('route' => 'posts.store')) !!}
-		{{ Form::label('title', 'Volunteer Title:') }}
-		{{ Form::text('title', null, array('class' => 'form-control')) }}
-		
-		
-		{{ Form::select('alumni', ['Admin' => 'Administrator', 'Alum' => 'Alumni'], 'Alum') }}
-		
-		{{ Form::label('body', "How would you like to help DePaul school?:") }}
-	    {{ Form::textarea('body', null, array('class' => 'form-control' ) ) }}
-		
-		
-		{{ Form::label('donation', "Description of volunteer activity or donation:") }}
-	    {{ Form::textarea('donation', null, array('class' => 'form-control' ) ) }}
-		
-		{{ Form::submit('Enter post into the database', array('class' => 'btn btn-success btn-lg btn-block', 'style' => 'margin-top: 20px;'     )) }}
-		
-	{!! Form::close() !!}
-	
-	</div>
-</div>
+            <h1><img class="img img-fluid img-rounded mx-auto" src="{{url('/images/lion.jpg')}}" alt="Image"/>Volunteer information - Create New Post</h1>
+            <p>You may enter a description of what the volunteer is donating to the school</p>
+            <hr>
+
+            {!! Form::open(array('route' => 'posts.store')) !!}
+            {{ Form::label('title', 'Volunteer Title:') }}
+            {{ Form::text('title', null, array('class' => 'form-control')) }}
+
+
+            {{ Form::select('alumni', ['Admin' => 'Administrator', 'Alum' => 'Alumni'], 'Alum') }}
+
+            {{ Form::label('body', "How would you like to help DePaul school?:") }}
+            {{ Form::textarea('body', null, array('class' => 'form-control' ) ) }}
+
+
+            {{ Form::label('donation', "Description of volunteer activity or donation:") }}
+            {{ Form::textarea('donation', null, array('class' => 'form-control' ) ) }}
+
+            {{ Form::submit('Enter post into the database', array('class' => 'btn btn-success btn-lg btn-block', 'style' => 'margin-top: 20px;'     )) }}
+
+            {!! Form::close() !!}
+
+        </div>
+    </div>
 
 @endsection
