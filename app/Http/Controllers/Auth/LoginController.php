2da61323cebc4f92c945302873a758e7f030ed67
stephenhartman <stephen.c.hartman@gmail.com>
2017-10-31 16:29:58 -0400

    Add Oauth tables

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -38,12 +38,13 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
     }
 
+
     /**
      * Redirect the user to the Google authentication page.
      *
      * @return Response
      */
-    public function redirectToProvider()
+    public function redirectToGoogleProvider()
     {
         return Socialite::driver('google')->redirect();
     }
@@ -53,10 +54,74 @@ class LoginController extends Controller
      *
      * @return Response
      */
-    public function handleProviderCallback()
+    public function handleGoogleProviderCallback()
     {
-        $user = Socialite::driver('google')->user();
 
-        // $user->token;
+        $user_by_email = User::where('email',  Socialite::driver('google')->stateless()->user()->email )->first();
+        $user_by_id = OAuthUser::where('oauth_id', Socialite::driver('google')->stateless()->user()->id )->first();
+
+        $socialite = Socialite::driver('google)')->stateless()->user();
+
+        if ($user_by_email) {
+            $user = $user_by_email;
+        } else if ($user_by_id) {
+            $user = $user_by_id;
+        } else {
+            // Create User
+            $user = User::create([
+                'name' => $socialite->getName(),
+                'email' => $socialite->getEmail(),
+            ]);
+        }
+
+        $oauthUser = OAuthUser::firstOrNew([
+            'user_id' => $user->id,
+            'oauth_id' => $socialite->getId(),
+        ]);
+
+        $oauthUser->user_id = $user->id;
+        $oauthUser->oauth_id = $socialite->getId();
+        $oauthUser->access_token = $socialite->token;
+        $oauthUser->refresh_token = $socialite->refreshToken;
+        $oauthUser->oauth_driver_id = OAuthDriver::where('name', $provider)->first()->id;
+        $oauthUser->save();
+
+        return response()->json([
+            'access_token' => $user->createToken('API Token')->accessToken,
+        ]);
+    }
+
+    /**
+     * Redirect the user to the Facebook authentication page.
+     *
+     * @return Response
+     */
+    public function redirectToFacebookProvider()
+    {
+        return Socialite::driver('facebook')->redirect();
+    }
+
+    /**
+     * Obtain the user information from Facebook.
+     *
+     * @return Response
+     */
+    public function handleFacebookProviderCallback()
+    {
+
+        $user_by_email = User::where('email',  Socialite::driver('facebook')->stateless()->user()->email )->first();
+        $user_by_id = OAuthUser::where('oauth_id', Socialite::driver('facebook')->stateless()->user()->id )->first();
+
+        if ($user_by_id) {
+            $user = User::find($user_by_id->user_id);
+            // return passport access_token
+        }
+        elseif ($user_by_email) {
+            return Response()->json(['error' => 324, 'message' => 'You already have an account please signin using login credentials and link this social account to use in future.']);
+        }
+        else {
+            // Create user
+            // if successful then return passport access_token
+        }
     }
 }
