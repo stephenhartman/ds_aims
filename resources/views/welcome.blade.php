948012a38b1d464ffa0c73c75c08f522a992ae32
stephenhartman <stephen.c.hartman@gmail.com>
2017-11-16 20:13:20 -0500

    More old styling gutted

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,36 +1,35 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>@yield('title', config('app.name'))</title>
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
-    </head>
-        <div class="flex-center position-ref full-height">
-            <navbar class="navbar navbar-default navbar-fixed-top">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>@yield('title', config('app.name'))</title>
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+</head>
+<navbar class="navbar navbar-default navbar-fixed-top">
+    @if (Route::has('login'))
+        <div class="top-right links">
+            @auth
+                <a href="{{ url('/home') }}">Home</a>
+                @else
+                    <a href="{{ route('login') }}">Login</a>
                     @endauth
-                </div>
-            @endif
-            </navbar>
-            <div class="content">
-                <a href="{{ url('/home') }}" style="color:black; text-decoration:none">
-                    <div class="title">
-                        DePaul School Alumni Outreach System
-                    </div>
-                    <br>
-                    <img class="img img-fluid img-responsive" src="{{url('/images/depaul.jpg')}}" alt="Image">
-                </a>
-            </div>
         </div>
-    </body>
+    @endif
+</navbar>
+<body>
+<div class="content">
+    <a href="{{ url('/home') }}" style="color:black; text-decoration:none">
+        <div class="title">
+            DePaul School Alumni Outreach System
+        </div>
+        <br>
+        <img class="img img-fluid img-responsive" src="{{url('/images/depaul.jpg')}}" alt="Image">
+    </a>
+</div>
+</body>
 </html>
