0ea6854ec11efd4f95a0350c5bf9a9b632c90031
stephenhartman <stephen.c.hartman@gmail.com>
2017-11-16 19:09:03 -0500

    Fix the footer w/ sprucing

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,5 @@
 <div class="container">
-    <br><br>
-    <nav class="navbar">
+    <nav class="footer navbar-brand navbar-default navbar-fixed-bottom">
         <div id="copyright" class="text-right">&copy; Copyright 2017 Initech</div>
     </nav>
 </div>
