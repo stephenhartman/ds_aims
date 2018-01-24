40cf4ec9d82102c4b174e385ea489003d58fc2bb
stephenhartman <stephen.c.hartman@gmail.com>
2018-01-24 13:27:42 -0500

    Apply hr .posts

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,16 +9,15 @@
                 <div class="panel panel-default">
                     <div class="panel-heading">
                         <div class="row">
-                            <div class="col-md-2">
-                                <div class="panel-title">Dashboard</div>
+                            <div class="col-md-6">
+                                <h3>Latest Posts</h3>
                             </div>
                         </div>
                     </div>
-                    <div class="panel-body">
-                        <h3>The latest news from The DePaul School</h3>
-                    </div>
+                    <div>
+                        <br>
                     @include('layouts.posts', ['posts' => $posts])
-                    <hr>
+                    </div>
                     <div class="row">
                         <div class="col-md-8 col-md-offset-2">
                             <a class="btn btn-primary btn-block" href="{{ URL::to('posts') }}">More Posts</a>
