d4c2f476d9bf06943cc95bb0dd651463233265d3
stephenhartman <stephen.c.hartman@gmail.com>
2018-01-24 13:27:42 -0500

    Apply hr .posts

--- resources/views/layouts/posts.blade.php
+++ resources/views/layouts/posts.blade.php
@@ -1,14 +1,17 @@
 <div>
     @foreach ($posts as $post)
         <div class="row">
-            <div class="container-fluid">
+            <div class="col-md-10 col-md-offset-1">
+                <p class="h4">
+                    {{ $post->title }}
+                    <span class="text-muted pull-right">
+                            {{ date('M j, Y', strtotime($post->created_at)) }}
+                        </span>
+                </p>
                 <hr>
-                <div class="col-md-8 col-md-offset-2">
-                    <p class="h4 text-center">{{ $post->title }}</p>
-                    <p class="text-muted text-right">{{ date('M j, Y h:ia', strtotime($post->created_at)) }}</p>
-                    <p>{{ $post->body }}</p>
-                </div>
+                <p>{!! $post->body !!}</p>
             </div>
         </div>
+        <hr class="posts">
     @endforeach
 </div>
