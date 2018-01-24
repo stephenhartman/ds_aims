9cbee8afbfa9056bb0589413c033eaeb04613767
stephenhartman <stephen.c.hartman@gmail.com>
2018-01-24 10:04:34 -0500

    Reformat posts index and optimize forms

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,18 +2,29 @@
 
 @section('title', 'Create Post')
 
-
-
-
+@push('styles')
 	{!! Html::style('css/parsley.css') !!}
 	{!! Html::style('css/select2.min.css') !!}
+@endpush
+
+@push('scripts')
 	<script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
-
 	<script>
-		tinymce.init ({ selector: 'textarea', height: 500, theme: 'modern', plugins: [ 'advlist autolink lists link image charmap print preview hr anchor pagebreak', 'searchreplace wordcount visualblocks visualchars code fullscreen', 'insertdatetime media nonbreaking save table contextmenu directionality', 'emoticons template paste textcolor colorpicker textpattern imagetools codesample toc' ], toolbar1: 'undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image', toolbar2: 'print preview media | forecolor backcolor emoticons | codesample', image_advtab: true, templates: [ { title: 'Test template 1', content: 'Test 1' }, { title: 'Test template 2', content: 'Test 2' } ], content_css: [ '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i', '//www.tinymce.com/css/codepen.min.css' ] });﻿
+		tinymce.init ({
+            selector: 'textarea',
+            height: 500, theme: 'modern',
+            plugins: [ 'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+                'searchreplace wordcount visualblocks visualchars code fullscreen',
+                'insertdatetime media nonbreaking save table contextmenu directionality',
+                'emoticons template paste textcolor colorpicker textpattern imagetools codesample toc' ],
+            toolbar1: 'undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+            toolbar2: 'print preview media | forecolor backcolor emoticons | codesample',
+            image_advtab: true,
+            templates: [ { title: 'Test template 1', content: 'Test 1' },
+                { title: 'Test template 2', content: 'Test 2' } ],
+            content_css: ['//www.tinymce.com/css/codepen.min.css' ] });
 	</script>
-
-
+@endpush
 
 @section('content')
     <div class ="row">
