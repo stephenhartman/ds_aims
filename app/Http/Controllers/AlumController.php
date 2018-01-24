cee06c890ee962c2b343c8e32698e9809591b420
stephenhartman <stephen.c.hartman@gmail.com>
2018-01-24 14:28:22 -0500

    Add alum controller methods for store and create

--- app/Http/Controllers/AlumController.php
+++ app/Http/Controllers/AlumController.php
@@ -3,18 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Alum;
+use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 
 class AlumController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+
+    public function __construct()
     {
-        //
+        $this->middleware('auth');
     }
 
     /**
@@ -24,7 +23,16 @@ class AlumController extends Controller
      */
     public function create()
     {
-        //
+        $user_id = Auth::id();
+        $alum =  Alum::where('user_id', $user_id);
+
+        if ($alum !== null)
+        {
+            Session::flash('error', 'You have already created an alumni account.  Please edit your existing account.');
+            return redirect()->route('home');
+        }
+        else
+            return view('users.alumni.create');
     }
 
     /**
@@ -35,7 +43,29 @@ class AlumController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        //validate
+        $this->validate($request, array(
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'loyal_lion' => 'required',
+        ));
+        //store
+        $alum = new Alum;
+        $alum->user_id = Auth::id();
+        $alum->first_name = $request->first_name;
+        $alum->last_name = $request->last_name;
+        $alum->phone_number = $request->cell_phone;
+        $alum->social_pref = $request->social_pref;
+        $alum->street_address = $request->street;
+        $alum->city = $request->city;
+        $alum->state = $request->state;
+        $alum->zipcode = $request->zipcode;
+        $alum->loyal_lion = $request->loyal_lion;
+        $alum->save();
+
+        Session::flash('success', 'Your alumni account was successfully created!');
+        return redirect()->route('users.alum.show', $alum->id);
+
     }
 
     /**
@@ -68,17 +98,6 @@ class AlumController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, Alum $alum)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Alum  $alum
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Alum $alum)
     {
         //
     }
