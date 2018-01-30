5c9c012c11db5f1036c8999be0f5ead3b8fa93e2
stephenhartman <stephen.c.hartman@gmail.com>
2018-01-30 11:06:35 -0500

    Alumni create view and change alumni id to increments

--- database/migrations/2018_01_23_122335_create_alumni_table.php
+++ database/migrations/2018_01_23_122335_create_alumni_table.php
@@ -13,8 +13,8 @@ class CreateAlumniTable extends Migration
      */
     public function up()
     {
-        Schema::table('alumni', function (Blueprint $table) {
-           $table->integer('id')->unsigned();
+        Schema::create('alumni', function (Blueprint $table) {
+           $table->increments('id');
            $table->integer('user_id')->unsigned();
            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
            $table->string('first_name');
