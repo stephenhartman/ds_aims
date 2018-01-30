8f26fd0200b06e5814794aa6812574e2db5ada1e
kbell140 <kbell140@gmail.com>
2018-01-30 14:03:49 -0500

    - full calendar implemented - events model updated - user sign up migrations/model created - user sign up controller and views made - to do: auth problems with non admin users

--- app/Http/Controllers/User_Sign_Up_Controller.php
+++ app/Http/Controllers/User_Sign_Up_Controller.php
@@ -17,8 +17,8 @@ class User_Sign_Up_Controller extends Controller
 {
     public function index()
     {
-
     }
+
 
     public function create(Event $event)
     {
@@ -40,4 +40,21 @@ class User_Sign_Up_Controller extends Controller
         return redirect()->route('events.index');
     }
 
+    public function show(Event $event, User_Sign_Up $enroll)
+    {
+        //$event = Event::where('id',$event_id)->first();
+        return view('events.user_sign_up.show', compact('event','enroll'));
+    }
+
+    public function edit(Event $event, User_Sign_Up $enroll)
+    {
+        //$event = Event::where('id',$event_id)->first();
+        return view('events.user_sign_up.edit', compact('event','enroll'));
+    }
+
+    public function destroy(Event $event, User_Sign_Up $sign_Up)
+    {
+        $sign_Up->delete();
+        return redirect()->route('events.index');
+    }
 }
--- database/migrations/2018_01_26_124552_create_user_sign_up_table.php
+++ database/migrations/2018_01_26_124552_create_user_sign_up_table.php
@@ -13,7 +13,7 @@ class CreateUserSignUpTable extends Migration
      */
     public function up()
     {
-        Schema::create('user_sign_up', function (Blueprint $table) {
+        Schema::create('user_sign_ups', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id');
             $table->integer('event_id');
