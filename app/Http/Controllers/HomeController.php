0c968386fb13b2c56cc0ba06576d9ebcc982773d
stephenhartman <stephen.c.hartman@gmail.com>
2018-01-30 12:19:56 -0500

    Add home redirect if no alumnus record

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Alumnus;
 use Illuminate\Http\Request;
-
 use App\Post;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 
 class HomeController extends Controller
 {
@@ -25,11 +27,20 @@ class HomeController extends Controller
      */
     public function index(Request $request)
     {
-		$posts = Post::orderBy('updated_at', 'desc')->paginate(3);
-    	
-        if ($request->user()->hasRole('admin'))
-            return view('admin.home')->withPosts($posts);
+        $user = $request->user();
+        $alum =  Alumnus::where('user_id', $user->id)->get();
+
+        if ($alum->isEmpty() && $request->user()->hasRole('alumni'))
+            return view('users.alumni.create', compact('user'));
         else
-            return view('home')->withPosts($posts);
+        {
+            Session::flash('success', 'You have successfully logged in!  Welcome to the DePaul Alumni Outreach System!');
+            $posts = Post::orderBy('updated_at', 'desc')->paginate(3);
+
+            if ($request->user()->hasRole('admin'))
+                return view('admin.home')->withPosts($posts);
+            else
+                return view('home')->withPosts($posts);
+        }
     }
 }
