5603b1c563a70c82a9f2bf97f0df2b5079448a17
stephenhartman <stephen.c.hartman@gmail.com>
2018-02-01 19:37:36 -0500

    Refactor migrations id to increments

--- database/migrations/2018_01_24_123608_create_educations_table.php
+++ database/migrations/2018_01_24_123608_create_educations_table.php
@@ -14,7 +14,7 @@ class CreateEducationsTable extends Migration
     public function up()
     {
         Schema::create('educations', function (Blueprint $table) {
-            $table->integer('id')->unsigned();
+            $table->increments('id');
             $table->string('type');
             $table->string('diploma');
             $table->string('school');
