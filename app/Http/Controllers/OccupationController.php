b2feb174e1a3475eac9f498a032d8e01923e1335
stephenhartman <stephen.c.hartman@gmail.com>
2018-02-01 20:23:29 -0500

    Update controllers

--- app/Http/Controllers/OccupationController.php
+++ app/Http/Controllers/OccupationController.php
@@ -2,31 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use App\Occupation;
 use App\Alumnus;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+use Purifier;
 
 class OccupationController extends Controller
 {
     /**
      * Show the form for creating a new resource.
      *
-     * @param  \App\Alumnus  $alum
+     * @param User $user
+     * @param  \App\Alumnus  $alumnus
      * @return \Illuminate\Http\Response
      */
-    public function create(Alumnus $alum)
+    public function create(User $user, Alumnus $alumnus)
     {
-        return view('users.alumni.occupation.create', compact('alum'));
+        return view('users.alumni.occupation.create', compact('user','alumnus'));
     }
 
     /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Alumnus  $alum
+     * @param User @user
+     * @param  \App\Alumnus  $alumnus
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request, Alumnus $alum)
+    public function store(Request $request, User $user, Alumnus $alumnus)
     {
         $this->validate($request, array(
             'organization' => 'required',
@@ -36,26 +41,26 @@ class OccupationController extends Controller
 
         $occupation = new Occupation();
 
-        $occupation->alumni_id = $alum->id;
+        $occupation->alumni_id = $alumnus->id;
         $occupation->organization = $request->organization;
         $occupation->position = $request->position;
         $occupation->start_year = $request->start_year;
         $occupation->end_year = $request->end_year;
-        $occupation->testimonial = $request->testimonial;
+        $occupation->testimonial = Purifier::clean($request->testimonial);
         $occupation->save();
 
         Session::flash('success', 'The occupation milestone was successfully created!');
-        return redirect()->route('users.alumni.show', compact('alum'));
+        return redirect()->route('users.alumni.milestones.index', compact('user', 'alumnus'));
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Alumnus  $alum
+     * @param  \App\Alumnus  $alumnus
      * @param  \App\Occupation  $occupation
      * @return \Illuminate\Http\Response
      */
-    public function show(Alumnus $alum, Occupation $occupation)
+    public function show(Alumnus $alumnus, Occupation $occupation)
     {
         //
     }
@@ -63,48 +68,55 @@ class OccupationController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Alumnus  $alum
+     * @param  \App\Alumnus  $alumnus
      * @param  \App\Occupation  $occupation
      * @return \Illuminate\Http\Response
      */
-    public function edit(Alumnus $alum, Occupation $occupation)
+    public function edit(Alumnus $alumnus, Occupation $occupation)
     {
-        return view('users.alumni.occupation.edit', compact('alum', 'occupation'));
+        return view('users.alumni.occupation.edit', compact('alumnus', 'occupation'));
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Alumnus  $alum
+     * @param  \App\Alumnus  $alumnus
      * @param  \App\Occupation  $occupation
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Alumnus $alum, Occupation $occupation)
+    public function update(Request $request, User $user, Alumnus $alumnus, Occupation $occupation)
     {
+        $this->validate($request, array(
+            'organization' => 'required',
+            'position' => 'required',
+            'start_year' => 'required'
+        ));
+
         $occupation->organization = $request->organization;
         $occupation->position = $request->position;
         $occupation->start_year = $request->start_year;
         $occupation->end_year = $request->end_year;
-        $occupation->testimonial = $request->testimonial;
+        $occupation->testimonial = Purifier::clean($request->testimonial);
         $occupation->save();
 
         Session::flash('success', 'The education milestone was successfully saved.');
-        return redirect()->route('users.alumni.show', compact('alum'));
+        return redirect()->route('users.alumni.show', compact('user', 'alumnus'));
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Alumnus  $alum
+     * @param User $user
+     * @param  \App\Alumnus  $alumnus
      * @param  \App\Occupation  $occupation
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Alumnus $alum, Occupation $occupation)
+    public function destroy(User $user, Alumnus $alumnus, Occupation $occupation)
     {
         $occupation->delete();
 
         Session::flash('alert', 'The occupation milestone was succesfully deleted.');
-        return redirect()->route('users.alumni.show', compact('alum'));
+        return redirect()->route('users.alumni.milestones.index', compact('user', 'alumnus'));
     }
 }
