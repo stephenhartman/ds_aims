e6a3630442325b4bb0223db7f89b70d4e9a2a3db
stephenhartman <stephen.c.hartman@gmail.com>
2018-02-05 17:50:05 -0500

    Add footer tweaks

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,17 +1,44 @@
-<div class="container">
+<div class="container-fluid footer">
     <nav class="navbar-brand navbar-nav navbar-default footer" style="float:none">
-        <div class="flex-center">
-            <a href="https://www.facebook.com/depaulschool" target="_blank" title="Facebook Profile">
-                <span class="icon fa fa-facebook-square social"></span>
-            </a>
-            <a href="https://www.twitter.com/depaulschooljax" target="_blank" title="Twitter Profile">
-                <span class="icon fa fa-twitter-square social"></span>
-            </a>
-            <a href="http://www.depaulschool.com" target="_blank" title="Home Page">
-                <span>
-                    <img class="social" src="https://mediaprocessor.websimages.com/fit/1920x1920/www.depaulschool.com/Large DePaul Lion Head Silhouette Facing Right.png" height="28px">
-                </span>
-            </a>
+        <div class="row">
+            <div class="col-md-3 col-md-offset-3">
+                <div class="row">
+                    <div class="col-md-12">
+                        <a class="btn" href="https://www.facebook.com/depaulschool" target="_blank" title="Facebook Profile">
+                            <span class="icon fa fa-facebook-square social"></span>
+                            Facebook
+                        </a>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <a class="btn" href="https://www.twitter.com/depaulschooljax" target="_blank" title="Twitter Profile">
+                            <span class="icon fa fa-twitter-square social"></span>
+                            Twitter
+                        </a>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <a class="btn" href="http://www.depaulschool.com" target="_blank" title="Home Page">
+                                <span>
+                                    <img class="social" src="https://mediaprocessor.websimages.com/fit/1920x1920/www.depaulschool.com/Large DePaul Lion Head Silhouette Facing Right.png" height="28px">
+                                </span>
+                            Home Page
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card flex-center">
+                    <div class="card-block">
+                        <h4 class="card-title"><strong>DePaul School of Northeast Florida</strong></h4>
+                        <h5 class="card-title">We Teach The Way They Learn©</h5>
+                        <h5 class="card-title">3044 S. San Pablo Road</h5>
+                        <h5 class="card-title">Jacksonville, FL 32224</h5>
+                    </div>
+                </div>
+            </div>
         </div>
     </nav>
 </div>
