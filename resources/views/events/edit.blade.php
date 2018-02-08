bb3bbbc52edc88c463c0a7283d77a22317ec1c74
kbell140 <kbell140@gmail.com>
2018-02-08 14:55:14 -0500

    repeating events controller, events form validation rules, changes to model and migrations for improved usage

--- resources/views/events/edit.blade.php
+++ resources/views/events/edit.blade.php
@@ -5,7 +5,7 @@
 @section('content')
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-            <h1><img class="img img-fluid img-rounded mx-auto" src="{{url('/images/lion.jpg')}}" alt="Image"/>Create new event</h1>
+            <h1><img class="img img-fluid img-rounded mx-auto" src="{{url('/images/lion.jpg')}}" alt="Image"/>Update event</h1>
             {{ Form::model($event, ['route' => ['events.update', $event->id], 'method' => 'PUT']) }}
             {{Form::label('event_title','Event Title')}}
             {{Form::text('event_title', $event->title, array('class' => 'form-control', 'required' => 'required') )}}
@@ -32,11 +32,11 @@
             <div class="well">
                 <div class="row">
                     <div class="col-md-6">
-                        {{ Html::linkRoute('events.index', 'Cancel', array($event->id), array('class' => "btn btn-danger btn-lg btn-block")) }}
-                    </div>
-                    <div class="col-md-6">
                         {{ Form::submit('Save Changes', array('class' => 'btn btn-success btn-lg btn-block')) }}
                         {{ Form::close() }}
+                    </div>
+                    <div class="col-md-6">
+                        {{ Html::linkRoute('events.index', 'Cancel', array($event->id), array('class' => "btn btn-danger btn-lg btn-block")) }}
                     </div>
                 </div>
             </div>
