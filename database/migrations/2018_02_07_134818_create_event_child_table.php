a39abae19b1bc8617e3a15171cbb785b8b1c01b9
kbell140 <kbell140@gmail.com>
2018-02-07 19:12:56 -0500

    repeating events

---
+++ database/migrations/2018_02_07_134818_create_event_child_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventChildTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events_child', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('parent_id');
+            $table->text('title');
+            $table->dateTime('start_date');
+            $table->dateTime('end_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('event_child');
+    }
+}
