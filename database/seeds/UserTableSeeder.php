8b031b6bafc8aa9e1d96ea440152a8e2d51ed96a
stephenhartman <stephen.c.hartman@gmail.com>
2018-02-16 10:57:12 -0500

    Add factories to UserTablesSeeder

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -19,16 +19,23 @@ class UserTableSeeder extends Seeder
         $admin_user = User::where('email', 'admin@example.com')->first();
         if ($admin_user === null) {
             $admin = new User();
-            $admin->name = 'Admin Istrator';
+            $admin->name = 'Administrator Person';
             $admin->email = 'admin@example.com';
             $admin->password = bcrypt('secret');
             $admin->save();
             $admin->roles()->attach($role_admin);
         }
 
-        $users = factory(App\User::class, 100)->create()->each(function ($user) {
-            $user->alumnus()->save(factory(App\Alumnus::class)->make());
-        });
+        $users = factory(App\User::class, 100)->create();
+
+        foreach($users as $user) {
+            $alumnus = $user->alumnus()->save(factory(App\Alumnus::class)->make());
+            $alumnus->educations()->save(factory(App\Education::class)->make());
+            $alumnus->educations()->save(factory(App\Education::class)->make());
+            $alumnus->occupations()->save(factory(App\Occupation::class)->make());
+            $alumnus->occupations()->save(factory(App\Occupation::class)->make());
+        }
+
         foreach($users as $user)
         {
             $user->roles()->attach($role_alumni);
