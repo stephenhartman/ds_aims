34f6661125c906209832bccc5af318ba940b5951
stephenhartman <stephen.c.hartman@gmail.com>
2018-02-15 19:17:59 -0500

    Fix titles

--- resources/views/admin/show.blade.php
+++ resources/views/admin/show.blade.php
@@ -8,7 +8,7 @@
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
                     <div class="panel-heading">
-                        Admin User Show
+                        Admin Profile
                     </div>
                     <div class="panel-body">
                         You are logged in!
