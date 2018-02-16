68e7a77d21e942b5cc796daad1be21d8c3eb84c5
stephenhartman <stephen.c.hartman@gmail.com>
2018-02-16 16:18:41 -0500

    Update seeders, factory and database for users

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,6 +22,7 @@ $factory->define(App\User::class, function (Faker $faker) {
         'email' => $faker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
-        'last_login_at' => $faker->dateTimeBetween($startDate = '-1 years', $endDate = 'now', $timezone = null)
+        'last_login_at' => $faker->dateTimeBetween($startDate = '-1 years', $endDate = 'now', $timezone = null),
+        'verified' => 1,
     ];
 });
