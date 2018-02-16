5217e036d32776a9346faebb98c420287be676dd
stephenhartman <stephen.c.hartman@gmail.com>
2018-02-16 16:20:03 -0500

    Add markdown mailer layout and vendor mailers

---
+++ resources/views/vendor/laravel-user-verification/user-verification.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+<!-- Main Content -->
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">{!! trans('laravel-user-verification::user-verification.verification_error_header') !!}</div>
+                <div class="panel-body">
+                    <span class="help-block">
+                        <strong>{!! trans('laravel-user-verification::user-verification.verification_error_message') !!}</strong>
+                    </span>
+                    <div class="form-group">
+                        <div class="col-sm-6">
+                            <a href="{{url('/resend-verification')}}" class="btn btn-primary">
+                                Resend verification Email
+                            </a>
+                        </div>
+                        <div class="col-sm-6">
+                            <a href="{{url('/')}}" class="btn btn-primary">
+                                {!! trans('laravel-user-verification::user-verification.verification_error_back_button') !!}
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
