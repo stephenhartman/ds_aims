a03943c72fa71cf9466a11d784f56f4ddaab909a
stephenhartman <stephen.c.hartman@gmail.com>
2018-02-21 15:33:09 -0500

    Change error view

--- resources/views/vendor/laravel-user-verification/user-verification.blade.php
+++ resources/views/vendor/laravel-user-verification/user-verification.blade.php
@@ -2,31 +2,33 @@
 
 <!-- Main Content -->
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">{!! trans('laravel-user-verification::user-verification.verification_error_header') !!}</div>
-                <div class="panel-body">
-                    <div class="alert alert-danger" role="alert">
-                        <strong>{!! trans('laravel-user-verification::user-verification.verification_error_message') !!}</strong>
-                        Please check to see you confirmed the latest verification email.
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-primary">
+                    <div class="panel-heading">
+                        <div class="panel-title">{!! trans('laravel-user-verification::user-verification.verification_error_header') !!}</div>
                     </div>
-                    <div class="form-group">
-                        <div class="col-sm-6">
-                            <a href={{ route('resend-verification', Auth::id()) }} class="btn btn-primary btn-sm btn-block">
-                                Resend Verification
-                            </a>
+                    <div class="panel-body">
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <p>
+                                    Please check your email for the verification message.
+                                </p>
+                            </div>
                         </div>
-                        <div class="col-sm-6">
-                            <a href="{{url('/')}}" class="btn btn-warning btn-sm btn-block">
-                                {!! trans('laravel-user-verification::user-verification.verification_error_back_button') !!}
-                            </a>
+                        <div class="row">
+                            <div class="form-group">
+                                <div class="col-sm-12 text-center">
+                                    <a href="{{ route('resend-verification', array(Auth::id())) }}" class="btn btn-primary">
+                                        Resend verification Email
+                                    </a>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
