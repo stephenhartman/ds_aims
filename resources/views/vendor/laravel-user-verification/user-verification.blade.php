566be58030072116df4b22f218305c187ea690b2
stephenhartman <stephen.c.hartman@gmail.com>
2018-02-21 15:20:03 -0500

    Fix verification error view and button

--- resources/views/vendor/laravel-user-verification/user-verification.blade.php
+++ resources/views/vendor/laravel-user-verification/user-verification.blade.php
@@ -8,17 +8,18 @@
             <div class="panel panel-default">
                 <div class="panel-heading">{!! trans('laravel-user-verification::user-verification.verification_error_header') !!}</div>
                 <div class="panel-body">
-                    <span class="help-block">
+                    <div class="alert alert-danger" role="alert">
                         <strong>{!! trans('laravel-user-verification::user-verification.verification_error_message') !!}</strong>
-                    </span>
+                        Please check to see you confirmed the latest verification email.
+                    </div>
                     <div class="form-group">
                         <div class="col-sm-6">
-                            <a href="{{url('/resend-verification')}}" class="btn btn-primary">
-                                Resend verification Email
+                            <a href={{ route('resend-verification', Auth::id()) }} class="btn btn-primary btn-sm btn-block">
+                                Resend Verification
                             </a>
                         </div>
                         <div class="col-sm-6">
-                            <a href="{{url('/')}}" class="btn btn-primary">
+                            <a href="{{url('/')}}" class="btn btn-warning btn-sm btn-block">
                                 {!! trans('laravel-user-verification::user-verification.verification_error_back_button') !!}
                             </a>
                         </div>
