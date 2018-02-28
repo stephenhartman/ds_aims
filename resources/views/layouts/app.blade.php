ab88ce1e4a960fde7b06e7ac192836d8a2d57d41
stephenhartman <stephen.c.hartman@gmail.com>
2018-02-28 11:18:38 -0500

    Change title on layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <title>@yield('title', config('app.name'))</title>
+        <title>@yield('title', 'DePaul Alumni Outreach')</title>
         {{ Html::favicon('favicon.ico') }}
         <!-- Styles -->
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
