065832758afb7ef9eb978477a0537e952473c2a1
stephenhartman <stephen.c.hartman@gmail.com>
2018-03-04 14:03:21 -0500

    Add Photos migration and model

---
+++ app/Photo.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Photo extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'photo_url', 'caption'
+    ];
+}
