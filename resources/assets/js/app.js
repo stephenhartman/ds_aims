c1ec139d61a74a6de96b752c2398ec67403964a1
stephenhartman <stephen.c.hartman@gmail.com>
2018-03-04 22:39:54 -0500

    Add normalize height js function for carousels, WIP

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -27,3 +27,61 @@ window.swal = require('sweetalert');
 //     Vue.config.debug = false;
 //     Vue.config.silent = true;
 // }
+
+// THIS COULD BE USED TO NORMALIZE CAROUSEL HEIGHTS
+// function normalizeSizesAllCarousels()
+// {
+//     //we loop all the present carousels
+//     jQuery('.carousel.slide').each(function()
+//     {
+//         normalizeSizesOneCarousel('#' + jQuery(this).attr('id'));
+//     });
+// }
+//
+// function normalizeSizesOneCarousel(idSelectorCarousel)
+// {
+//     var images = jQuery(idSelectorCarousel +  ' .carousel-inner .item img');
+//     var availableWidth = jQuery(idSelectorCarousel).innerWidth();
+//     var maxHeight = 0;
+//     var imageHeight = 0;
+//     var aspectRatio = 1; // width/height
+//     images.each(function(){
+//         aspectRatio = calcImgWidth(this)/calcImgHeight(this);
+//         imageHeight = availableWidth / aspectRatio;
+//         if (imageHeight > maxHeight)
+//             maxHeight = imageHeight;
+//     });
+//     jQuery (idSelectorCarousel).height(maxHeight);
+// }
+// function calcImgWidth(img)
+// {
+//     var width=img.width;
+//     if (width>0)
+//         return width;
+//     else
+//     {
+//         jQuery('body').append('<div id="tempContainer" style="visibility:hidden;position:absolute"></div>');
+//         jQuery('#tempContainer').append(img.clone().attr('id',false));
+//         width= jQuery('#tempContainer img').width;
+//         jQuery('#tempContainer').remove();
+//         return width;
+//     }
+// }
+//
+// function calcImgHeight(img)
+// {
+//     var height = img.height;
+//     if (height>0)
+//         return height;
+//     else
+//     {
+//         jQuery('body').append('<div id="tempContainer" style="visibility:hidden;position:absolute"></div>');
+//         jQuery('#tempContainer').append(img.clone().attr('id',false));
+//         height= jQuery('#tempContainer img').height;
+//         jQuery('#tempContainer').remove();
+//         return height;
+//     }
+// }
+//
+// jQuery(document).ready(function(){normalizeSizesAllCarousels();});
+// jQuery(window).on('resize orientationchange', function () {normalizeSizesAllCarousels();});
