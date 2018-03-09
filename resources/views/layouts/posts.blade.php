cc655cb7d97978e14e592f9530d4e9cca11bf982
stephenhartman <stephen.c.hartman@gmail.com>
2018-03-09 12:45:55 -0500

    Add post author photo to posts

--- resources/views/layouts/posts.blade.php
+++ resources/views/layouts/posts.blade.php
@@ -2,13 +2,25 @@
     @foreach ($posts as $post)
         <div class="row">
             <div class="col-md-10 col-md-offset-1 col-xs-10 col-xs-offset-1 col-sm-10 col-sm-offset-1">
-                <p class="h4">
-                    {{ $post->title }}
-                    <span class="text-muted pull-right">
-												By <strong>{{ $post->user->name }}</strong> on
-                        {{ date('M j, Y', strtotime($post->created_at)) }}
-											</span>
-                </p>
+                <div class="row">
+                    <h5>
+                        <div class="col-md-6 col-xs-12">
+                            <h4 class="pull-left">
+                                {{ $post->title }}
+                            </h4>
+                        </div>
+                        <div class="col-md-6 col-xs-12">
+                            <div class="text-muted text-right">
+                                @if ($post->user->photo_url !== null)
+                                    <img class="img-rounded text-right" height="50px" width="auto" src="{{ $post->user->photo_url }}">
+                                @endif
+                                <strong>{{ $post->user->name }}</strong>
+                                <br>
+                                Posted {{ date('M j, Y', strtotime($post->created_at)) }}
+                            </div>
+                        </div>
+                    </h5>
+                </div>
                 <hr>
                 <p>{!! $post->body !!}</p>
             </div>
--- resources/views/posts/load.blade.php
+++ resources/views/posts/load.blade.php
@@ -5,13 +5,25 @@
                 @if (Auth::user()->hasRole('admin'))
                     <a href="{{ route('posts.edit', $post->id) }}" class="btn btn-default btn-sm">Edit</a>
                 @endif
-                <p class="h4">
-                    {{ $post->title }}
-                    <span class="text-muted pull-right">
-												By <strong>{{ $post->user->name }}</strong> on
-                        {{ date('M j, Y', strtotime($post->created_at)) }}
-											</span>
-                </p>
+                <div class="row">
+                    <h5>
+                        <div class="col-md-6 col-xs-12">
+                            <h4 class="pull-left">
+                                {{ $post->title }}
+                            </h4>
+                        </div>
+                        <div class="col-md-6 col-xs-12">
+                            <div class="text-muted text-right">
+                                @if ($post->user->photo_url !== null)
+                                    <img class="img-rounded text-right" height="50px" width="auto" src="{{ $post->user->photo_url }}">
+                                @endif
+                                <strong>{{ $post->user->name }}</strong>
+                                <br>
+                                Posted {{ date('M j, Y', strtotime($post->created_at)) }}
+                            </div>
+                        </div>
+                    </h5>
+                </div>
                 <hr>
                 <p>{!! $post->body !!}</p>
             </div>
