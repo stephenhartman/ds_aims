d7fd3985dc4ea1181dd2a93fe5b00491f7052d54
stephenhartman <stephen.c.hartman@gmail.com>
2018-04-04 19:27:56 -0400

    Add parent_name condition to factory

--- database/factories/AlumniFactory.php
+++ database/factories/AlumniFactory.php
@@ -3,7 +3,8 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Alumnus::class, function (Faker $faker) {
-    return [
+
+    $alumnus = [
         'first_name' => $faker->firstName,
         'last_name' => $faker->lastName,
         'phone_number' => $faker->phoneNumber,
@@ -12,9 +13,14 @@ $factory->define(App\Alumnus::class, function (Faker $faker) {
         'city' => $faker->city,
         'state' => $faker->state,
         'zipcode' => $faker->postcode,
-        'year_graduated' => $faker->year,
+        'year_graduated' => $faker->numberBetween($min = 1980, $max = 2018),
         'volunteer' => $faker->randomElement($array = array(0,1)),
         'loyal_lion' => $faker->randomElement($array = array(0,1)),
+        'is_parent' => $faker->randomElement($array = array(0,1)),
         'initial_setup' => 1
     ];
+
+    if ($alumnus['is_parent'] == 1)
+        $alumnus['parent_name'] = $faker->name;
+    return $alumnus;
 });
