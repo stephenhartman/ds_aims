b6626c4c72de7820d23af11dc92ffc525a767150
stephenhartman <stephen.c.hartman@gmail.com>
2018-04-18 10:57:54 -0400

    Remove development seeds for final deployment

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,14 +16,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // Role comes before User seeder here.
         $this->call(RoleTableSeeder::class);
-        // User seeder will use the roles above created.
-        $this->call(UserTableSeeder::class);
 
-        //$events = factory(App\Event::class, 15)->create();
-        //$posts = factory(App\Post::class, 23)->create();
-        // Call inverse posts seeds
-        $this->call(PostsTableSeeder::class);
+        /*
+         * Development seeds
+         *
+         * User seeder will use the roles above created.
+         * $this->call(UserTableSeeder::class);
+         * $events = factory(App\Event::class, 15)->create();
+         * $posts = factory(App\Post::class, 23)->create();
+         * Call inverse posts seeds
+         * $this->call(PostsTableSeeder::class);
+         */
     }
 }
